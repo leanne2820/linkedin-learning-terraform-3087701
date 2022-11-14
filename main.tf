9f97d203d88535375ef84a1584573c23cf6a951e
leanne2820 <59751596+leanne2820@users.noreply.github.com>
2022-11-14 09:45:55 -0800

    fix: change references to vpc to blog_vpc

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   name = "blog_new"
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules = [ "http-80-tcp", "https-443-tcp" ]
   ingress_cidr_blocks = [ "0.0.0.0/0" ]
