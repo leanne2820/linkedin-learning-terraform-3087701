d6d53c7bb26a7b6f9b7d8ff21511e3fefb68f947
leanne2820 <59751596+leanne2820@users.noreply.github.com>
2022-11-14 11:48:40 -0800

    create prod env

---
+++ prod/main.tf
@@ -0,0 +1,12 @@
+## calling our module modules/blog (redirecting it so that TF looks for the config files in that dir)
+module "dev" {
+    source = "../modules/blog"
+
+    environment = {
+     name = "prod"   
+        network_prefix = "10.2"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1 
+}
