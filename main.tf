803945104f33be826a0eeb6f6bd357b947aaa844
leanne2820 <59751596+leanne2820@users.noreply.github.com>
2022-11-14 10:13:06 -0800

    fix error

--- main.tf
+++ main.tf
@@ -89,11 +89,10 @@ module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   name = "blog_new"
-  vpc_id = module.blog_vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = [ "http-80-tcp", "https-443-tcp" ]
   ingress_cidr_blocks = [ "0.0.0.0/0" ]
-
   egress_rules = [ "all-all" ]
   egress_cidr_blocks = [ "0.0.0.0/0" ]
 }
