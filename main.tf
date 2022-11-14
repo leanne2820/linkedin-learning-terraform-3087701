58bb22ca7fba10709f855b23508820ae60960e45
leanne2820 <59751596+leanne2820@users.noreply.github.com>
2022-11-13 22:32:43 -0800

    fix invalid block error

--- main.tf
+++ main.tf
@@ -37,31 +37,30 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_ port = 80
-  protocol = "tcp"
-  cidr_block = ["0.0.0.0/0"] # which networks we want to allow access from
-
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
+
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type = "ingress"
-  from_port = 443
-  to_ port = 443
-  protocol = "tcp"
-  cidr_block = ["0.0.0.0/0"] # which networks we want to allow access from
-
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_everything_out" {
-  type = "egress"
-  from_port = 0 # from and to port 0 means allow everything 
-  to_ port = 0
-  protocol = "-1" # allow all protocols
-  cidr_block = ["0.0.0.0/0"] # which networks we want to allow access from
 
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
